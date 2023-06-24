fcd7bf2638686853e1e318eeef295bfb2b8ae4cd
kohstall <christoph.kohstall@gmail.com>
2023-06-24 15:57:30 -0700

    mcu

---
+++ brain.py
@@ -0,0 +1,98 @@
+import math
+import random
+
+import speech_recognition as sr
+
+import cerebellum
+from commands import check_is_command, say
+from llm_fns import *
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+cerebellum = cerebellum.Cerebellum()
+current_position, touch, mode = cerebellum.move([-5, -5], 3, "continue")
+action_examples = base_action_examples
+# current_position, touch = "[0, 0]", "False"
+
+
+# class Cerebellum:
+#     def __init__(self):
+#         pass
+
+#     def move(self, command, velocity, stop):
+#         print(command, False)
+#         return (command, False)
+
+
+# cerebellum = Cerebellum()
+
+
+def speak(text):
+    say(text)
+    return None
+
+
+def validate_action(action_str):
+    """
+    Parse action_str to validate it is a well formed command for
+    the exec method
+    """
+
+
+while 1:
+    command = ""
+
+    with mic as source:
+        print("Listening for commands")
+        r.adjust_for_ambient_noise(source, duration=0.1)
+        audio = r.listen(source, phrase_time_limit=4)
+
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # once we have exhausted the available calls
+        # instead of `r.recognize_google(audio)`
+        command = r.recognize_google(audio).lower()
+        if not check_is_command(command):
+            print("ignoring command", command)
+            continue
+        # prompt = f"Did you meant to say: {command}? "
+        # say(prompt)
+
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print(
+            "Could not request results from Google Speech Recognition service; {0}".format(
+                e
+            )
+        )
+
+    if command == "":
+        continue
+
+    # command = input("State a command: ")
+
+    actions, action_example = generate_action(
+        command, current_position, touch, action_examples
+    )
+    if len(action_examples) > 5:
+        action_examples.pop(0)
+    action_examples.append(action_example)
+
+    for action in actions.split("\n"):
+        try:
+            out = None
+            exec_action = f"out = {action}"
+            print(exec_action)
+            exec(exec_action)
+            if out and len(out) > 1:
+                current_position, touch, mode = out
+                current_position = tuple(round(el, 2) for el in current_position)
+                if mode == "stop" and touch == 1:
+                    say(vocalize_found_object())
+                    break
+            print(current_position, touch, mode)
+        except Exception as e:
+            speak(what_went_wrong(actions, e))
---
+++ commands.py
@@ -0,0 +1,49 @@
+import os
+import random
+import stat
+import string
+
+from gtts import gTTS
+from pydub import AudioSegment
+from pydub.playback import play
+
+language = "en"
+
+
+def validate_command(command):
+    # check if command is valid
+    # return True or False
+    pass
+
+
+def check_is_command(audio_text):
+    SPECIAL_KEYWORDS = ["lilly", "lil", "lily", "smelly", "really", "lillian", "lydia", "meaning", "lee", "billy"]
+    if audio_text is None:
+        return False
+    if not isinstance(audio_text, str):
+        return False
+    if not any(word in audio_text for word in SPECIAL_KEYWORDS):
+        return False
+    return True
+
+
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.ascii_lowercase
+    result_str = "".join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+def generate_random_tmp_folder_name():
+    file_name = get_random_string(10)
+    return f"/tmp/{file_name}.mp3"
+
+
+def say(text):
+    myobj = gTTS(text=text, lang=language, slow=False, tld="com.au")
+    temp_name = generate_random_tmp_folder_name()
+    myobj.save(temp_name)
+    print("prepping to play")
+    song = AudioSegment.from_mp3(temp_name)
+    print("playing")
+    play(song)
